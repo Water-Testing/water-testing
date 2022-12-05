b46c1e44983cb1c5d42f7a90dd44f4b7dde7389f
VIbitoye <112020449+VIbitoye@users.noreply.github.com>
2022-12-05 15:55:56 -0500

    H

--- src/Components/Samples.js
+++ src/Components/Samples.js
@@ -2,81 +2,79 @@ import React from 'react'
 import { Link, useRevalidator } from 'react-router-dom'
 import {useState} from 'react'
 import data from './SAMPLE_DATA.json'
+import EditModal from './EditModal'
+import BootstrapTable from 'react-bootstrap-table-next'
+import { RiTextSpacing } from 'react-icons/ri';
 
 const Samples = () => {
-  //for loading in the mock data
+  //for rendering in the mock data
  const [tests, setTests] = useState(data);
-const [showTest, setShowTests] = useState(false);
-const sortById =()=>{
+ //state for viewing information of a sample
+const [openModal, setOpenModal] = useState(false);
+const handleOnClose = () => setOpenModal(false);
 
-  
-}
- const sortByDate = () =>{
-    
-}
+console.log(data);
+const showDetails = (n) =>{
+  return(data[n].testID
 
-const sortByStatus = () =>{
-    
+  )
 }
+//state for searching for a test
+const[search,setSearch] = useState("");
+console.log(search);
+
 
   return (
-    <div class = "w-screen h-screen overflow-x-auto">
-        <h1 class = " text-4xl p-7 "> Water Samples (Bacteria) </h1>
-        <div className="relative">
-    
-                        <span className="absolute inset-y-0 left-0 flex items-center py-4 space-y-10">
-                            <button
+    /* Header */
+    <div class = "w-screen h-[70rem] overflow-auto dark:bg-gray-800 ">
+      <div class = "fixed bg-white w-screen">
+        <h1 class = " text-2xl font-semibold p-5 px-12 dark:text-white dark:bg-gray-800"> Water Samples (Bacteria) </h1>
+        {/*contianer for search */}
+        <div className="  dark:bg-gray-800 px-[3.7rem] h-[5rem] py-2">   
+        <span className="absolute inset-y-0 left-0 flex items-center">
+                            <button 
                                 type="submit"
-                               className ="p-2.5 sfocus:outline-none focus:ring bg-slate-200 rounded-lg  hover:bg-gray-300 active: h-10 w-9"
+                               className =""
                             >
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    className="w-4 h-4"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    stroke="currentColor"
-                                    strokeWidth={4}
-                                    
-                                >
-                                    <path
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                                    />
-                                </svg>
+                              {/* contains the search icon*/}
+                                
                             </button>
               
                         </span>
-                        <input
+                        <input onChange ={(e) => setSearch(e.target.value)}
                             type="search"
                             name="Search"
                               
                             placeholder="Search..."
 
-                            className="w-full py-2 pl-10 text-sm rounded-sm focus:outline-none h-10 border-0"
-                        />                       
+                            className="w-[50rem]  py-2 ml-1 text-sm rounded-md focus:outline-none mb-8 h-10 border-2"
+                        />                    
                     </div>
-                
-      <table class = "w-full table-auto ">
-        <thead class = "bg-gray-50 border-b-2 border-gray-300">
-        <tr class = "text-left font-bold ">
-        <th onClick = {sortById} class = "p-3 w-[35rem] text-md font-semibold tracking-wide text-left px-[5rem]">Testing Kit ID</th>
-        <th  onClick = {sortByDate} class = "p-3 px-[4rem] w-96 text-md font-semibold tracking-wide text-left">Date Added</th>
-         <th onClick = {sortByStatus} class = "p-3 px-[10rem] text-md font-semibold tracking-wide text-left">Status</th>
+                </div>
+      <table class = "h-screen w-screen mt-[9rem]">  {/* table of sample tests*/}
+        <thead class = "py-[.5rem] fixed bg-gray-50 border-b-2 border-gray-300 dark:bg-gray-800 dark:text-white dark:border-gray-500 w-screen">
+        <tr class = "text-left font-bold  ">
+        <th class = "p-3 px-[11rem] w-[35rem] text-md font-semibold tracking-wide text-left "><button>Testing Kit ID</button></th>
+        <th  class = "p-3 px-[8rem] w-96 text-md font-semibold tracking-wide text-left"><button>Date Added</button></th>
+         <th class = "p-3 px-[17.6rem] text-md font-semibold tracking-wide text-left"><button>Status</button></th>
        </tr> 
        </thead>
-       <tbody>
-        {tests.map((test)=>(
-        <tr class = "bg-gray-50 hover:bg-gray-200">
-          <td class = "p-3 px-20 text-black font-semibold ">{test.testID}</td>
-          <td class = "p-3 px-16 text-black font-semibold ">{test.dateAdded}</td>
-          <td class = "p-3 px-[10rem] text-black font-semibold">{test.testStatus}</td>
-         
+       <tbody class = "dark:text-white">
+        {tests.filter((test)=>{
+          /* search function */ 
+          return search.toLowerCase() === '' ? test : test.testID.toLowerCase().includes(search) || test.dateAdded.toLowerCase().includes(search) || test.testStatus.toLowerCase().includes(search);
+        } ).map((test)=>(
+        <tr key ={test.id} onClick = {() => setOpenModal(true)} class = "bg-gray-50 h-[4.3rem] hover:bg-gray-200  dark:bg-gray-700 dark:hover:bg-gray-600 overflow-auto">
+          <td class = "p-3 px-[11rem] text-black font-semibold dark:text-white ">{test.testID}</td>
+          <td class = "p-3 px-[3rem] text-black font-semibold dark:text-white">{test.dateAdded}</td>
+          <td class = "p-3 px-[20rem] text-black font-semibold dark:text-white">{test.testStatus}</td>
         </tr>))}
         
-        </tbody>
+        </tbody>  
         </table>
-
+        {/* editModal */}
+        <EditModal onClose = {handleOnClose} visible = {openModal}/>
+     
     </div>
 
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}",
-],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "class",
   theme: {
     extend: {},
-  },
-  plugins: [ require('flowbite/plugin')],
+  },  
+  //Plugins for certain customizations
+  plugins: [ require('flowbite/plugin'), require('tailwind-scrollbar-hide')],
 }

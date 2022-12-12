7a5f94a68a3f9355f21cde7e535d79375422c7a1
VIbitoye <112020449+VIbitoye@users.noreply.github.com>
2022-12-12 17:21:53 -0500

    recent

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import {XYPlot, LineSeries} from 'react-vis';
 function Home() {
+
   return (
     <div class = " w-screen h-screen content-center px-[10rem] bg-white dark:text-white dark:bg-gray-800">
       <div class=" flex-1 max-w-7xl ml-11 p-12">
@@ -8,12 +10,11 @@ function Home() {
 
 
             <div class = 'flex justify-center py-[15rem] text-4xl'>In progress</div>
-   {/*<ul class=" grid grid-cols-1 gap-4 sm:grid-cols-1 md:grid-cols-1 md:gap-20">
-
-        <li class="bg-white rounded-lg shadow-xl"><div class="h-[45rem] text-center font-bold">PLACEHOLDER</div></li>
-        <li class="bg-white rounded-lg shadow-xl"><div class="h-[25rem] text-center font-bold">PLACEHOLDER</div></li>
-    
-  </ul> */}
+        <div>
+          Finished tests:
+          Peformance:
+        </div>
+ 
 </div>
     </div>
  )
